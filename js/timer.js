03e3122a3a832c221276914004885ecf4e34b5cb
junho <83113983+hoya838@users.noreply.github.com>
2023-12-04 09:17:31 +0900

    Add files via upload

---
+++ js/timer.js
@@ -0,0 +1,18 @@
+function dateTimer() { 
+    let now = new Date();
+    let year = new Date('January 1, 2024');
+    let diff = year - now;
+    let milliseconds = Math.floor(diff % 1000);
+    diff = diff / 1000;
+    let seconds = Math.floor (diff % 60);
+    diff = diff / 60;
+    let minutes = Math.floor(diff % 60);
+    diff = diff / 60;
+    let hours = Math.floor (diff % 24);
+    diff = diff / 24;
+    let days = Math.floor (diff);
+    let outStr = "이벤트 종료 까지 "+ days + '일 ' + hours + '시간 ' + minutes +'분 ' + seconds +'초 남았습니다.';
+    document.getElementById('main_timer').innerHTML = outStr;
+    setTimeout("dateTimer()",1000);
+}
+dateTimer();
